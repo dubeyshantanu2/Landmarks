bf5878cbb4188d0854497d33f57884cee9962efe
Shantanu Dubey <shantanu@founderandlightning.com>
2024-05-28 15:40:09 +0530

    Combining View using stacks
    To combine stacks, press control and click on the view which needs to be converted into vertical or horizontal stack, by selecting Embed in VStack or HStack.
    We can add new element by clicking + icon on top right.
    Place the items inside the VStack and Hstack according to design.
    For adding Space in between use Spacer()

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -1,22 +1,17 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by Shantanu Dubey on 28/05/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .foregroundColor(Color.green)
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Turtle Rock")
-                .font(.title)
-                .padding()
+        VStack(alignment: .leading){
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
         }
         .padding()
     }
