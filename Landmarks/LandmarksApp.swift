e98cccbfc69d0a23ad19e66adddefc35e5e1bb4b
Shantanu Dubey <shantanu@founderandlightning.com>
2024-05-28 10:32:02 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shantanu Dubey on 28/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
